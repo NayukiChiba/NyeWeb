97fa958ebe99097bf5486cfa535eef40951f2f4f
NayukiChiba <MekoNayuki@outlook.com>
2025-01-01 18:43:00 +0800

    update .gitignore to exclude .vscode directory while keeping extensions.json

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Header from './components/Header.vue'
+import Home from './views/Home.vue'
+</script>
+
+<template>
+  <el-container>
+    <Header />
+    <el-main>
+      <Home />
+    </el-main>
+  </el-container>
+</template>
+
+<style>
+.el-main {
+  padding: 0;
+}
+</style>
