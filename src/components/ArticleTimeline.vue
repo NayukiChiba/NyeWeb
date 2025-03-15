1d3fb121be3882c9b75120ef4869e44a09876a10
NayukiChiba <MekoNayuki@outlook.com>
2025-03-15 13:34:00 +0800

    新增折叠功能于文章时间线，优化文章展示逻辑

--- src/components/ArticleTimeline.vue
+++ src/components/ArticleTimeline.vue
@@ -3,25 +3,32 @@
     <template #header>
       <div class="card-header">
         <span>文章归档</span>
+        <el-icon class="collapse-icon" @click="isCollapsed = !isCollapsed" :title="isCollapsed ? '展开' : '收起'">
+          <arrow-up-bold v-if="!isCollapsed" />
+          <arrow-down-bold v-else />
+        </el-icon>
       </div>
     </template>
-    <el-timeline>
-      <el-timeline-item
-        v-for="article in sortedArticles"
-        :key="article.slug"
-        :timestamp="article.date"
-        placement="top"
-      >
-        <a class="timeline-link" @click.prevent="onArticleClick(article.slug)">
-          {{ article.title }}
-        </a>
-      </el-timeline-item>
-    </el-timeline>
+    <el-collapse-transition>
+      <el-timeline v-show="!isCollapsed">
+        <el-timeline-item
+          v-for="article in sortedArticles"
+          :key="article.slug"
+          :timestamp="article.date"
+          placement="top"
+        >
+          <a class="timeline-link" @click.prevent="onArticleClick(article.slug)">
+            {{ article.title }}
+          </a>
+        </el-timeline-item>
+      </el-timeline>
+    </el-collapse-transition>
   </el-card>
 </template>
 
 <script setup>
-import { computed } from 'vue'
+import { ref, computed } from 'vue'
+import { ArrowUpBold, ArrowDownBold } from '@element-plus/icons-vue'
 
 const props = defineProps({
   articles: {
@@ -31,6 +38,8 @@ const props = defineProps({
 })
 
 const emit = defineEmits(['scrollToArticle'])
+
+const isCollapsed = ref(false)
 
 const sortedArticles = computed(() => {
   return [...props.articles].sort((a, b) => new Date(b.date) - new Date(a.date))
@@ -48,8 +57,15 @@ const onArticleClick = (slug) => {
   top: 100px; /* Adjust based on your header height */
 }
 
-.card-header span {
+.card-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   font-weight: bold;
+}
+
+.collapse-icon {
+  cursor: pointer;
 }
 
 .timeline-link {
@@ -62,4 +78,3 @@ const onArticleClick = (slug) => {
   color: #409eff;
 }
 </style>
-
