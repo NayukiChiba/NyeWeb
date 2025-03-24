3d1951676bc8b9e2f2f031e5e16d79ab72d432e7
NayukiChiba <MekoNayuki@outlook.com>
2025-03-24 10:19:00 +0800

    添加Github提交热力图

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -15,4 +15,5 @@ export default defineConfig({
             '@': fileURLToPath(new URL('./src', import.meta.url))
         },
     },
+    envDir: '../', // Load .env files from the root directory
 })
